7e39117547ad68cfb6576c34cb565050893cb844
davidsekielyk <david.sekielyk@gm2dev.com>
2022-09-13 20:03:11 -0300

    added cypress tests and GitHub action config

---
+++ site/cypress/e2e/header.cy.js
@@ -0,0 +1,26 @@
+import { a } from '@react-spring/web'
+
+describe('Header', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('links to the correct pages', () => {
+    cy.getBySel('logo').click()
+    cy.location('pathname').should('eq', '/')
+
+    cy.getBySel('nav-link-search').click()
+    cy.location('pathname').should('eq', '/search')
+
+    cy.getBySel('nav-link-home-page').eq(0).click()
+    cy.location('pathname').should('eq', '/search/frontpage')
+  })
+
+  it('the search bar returns the correct search results', () => {
+    cy.getBySel('search-input').eq(0).type('reloj{enter}')
+
+    cy.getBySel('product-tag').within(() => {
+      cy.getBySel('product-name').should('contain', 'Reloj Citizen 1')
+    })
+  })
+})
